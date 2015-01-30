177aee634770b6b937c6779fd5ee5adb0dc833fe
Deayn Sadinov <sadinov88@gmail.com>
2015-01-30 13:03:29 +0200

    remove empty spaces in main class

--- src/task3/poolpattern/Main.java
+++ src/task3/poolpattern/Main.java
@@ -1,18 +1,12 @@
 package task3.poolpattern;
 
-
-
 /**
  * @author Deyan Sadinov <sadinov88@gmail.com>
  */
 public class Main {
   public static void main(String[] args) {
-
-
     Book book = new Book("Deyan Sadinov","How to be a good programmer");
-
     MyPoolObject<Book> pool = new MyPoolObject<>(book,3);
-
     pool.acquire();
     pool.acquire();
     pool.acquire();
@@ -22,12 +16,5 @@ public class Main {
 
     pool.acquire();
     pool.acquire();
-
-
-
-
-
-
-
   }
 }
